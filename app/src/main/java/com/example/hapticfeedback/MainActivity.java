ef5a188c661ef682221c8639e14cfca20b6eab6f
Jaydip.Nathani <jaydip.optimumbrew@gmail.com>
2020-07-24 13:56:17 +0530

    1. VibrationEffect issue resolved. 2. Work on How to turn on default setting (continue...)

--- app/src/main/java/com/example/hapticfeedback/MainActivity.java
+++ app/src/main/java/com/example/hapticfeedback/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.hapticfeedback;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
@@ -14,64 +18,86 @@ import android.view.MotionEvent;
 import android.view.SoundEffectConstants;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    Button feedback_off, feedback_on, default_Vibration, default_Vibration_sound, d_long_press, error, success, warning, light, medium, heavy, selection, vib_pattern1, vib_pattern2, vibrate_sound;
+    Button default_sound_vibration, feedback_off, feedback_on, default_Vibration, default_Vibration_sound, d_long_press, error, success, warning, light, medium, heavy, selection, vib_pattern1, vib_pattern2, vibrate_sound;
 
     Vibrator myVib;
     MediaPlayer player;
     int Action_code = 1;
+    boolean isisHapticFeedbackEnabled = false;
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        default_Vibration = (Button) findViewById(R.id.btn);
-        default_Vibration_sound = (Button) findViewById(R.id.btn1);
-        d_long_press = (Button) findViewById(R.id.d_long_press);
-        error = (Button) findViewById(R.id.error);
-        success = (Button) findViewById(R.id.success);
-        warning = (Button) findViewById(R.id.warning);
-        light = (Button) findViewById(R.id.light);
-        medium = (Button) findViewById(R.id.medium);
-        heavy = (Button) findViewById(R.id.heavy);
-        selection = (Button) findViewById(R.id.selection);
-        vib_pattern1 = (Button) findViewById(R.id.pattern1);
-        vib_pattern2 = (Button) findViewById(R.id.pattern2);
-        vibrate_sound = (Button) findViewById(R.id.vibrate_sound);
-        feedback_off = (Button) findViewById(R.id.feedback_off);
-        feedback_on = (Button) findViewById(R.id.feedback_ON);
+        default_Vibration = findViewById(R.id.btn);
+        default_Vibration_sound = findViewById(R.id.btn1);
+        d_long_press = findViewById(R.id.d_long_press);
+        error = findViewById(R.id.error);
+        success = findViewById(R.id.success);
+        warning = findViewById(R.id.warning);
+        light = findViewById(R.id.light);
+        medium = findViewById(R.id.medium);
+        heavy = findViewById(R.id.heavy);
+        selection = findViewById(R.id.selection);
+        vib_pattern1 = findViewById(R.id.pattern1);
+        vib_pattern2 = findViewById(R.id.pattern2);
+        vibrate_sound = findViewById(R.id.vibrate_sound);
+        feedback_off = findViewById(R.id.feedback_off);
+        feedback_on = findViewById(R.id.feedback_ON);
+        default_sound_vibration = findViewById(R.id.default_sound_vibration);
 
         myVib = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
         player = MediaPlayer.create(MainActivity.this, R.raw.mouseclickcom);
 
         Log.i(TAG, "onCreate: isVibration  " + myVib.hasVibrator());
-        Log.i(TAG, "onCreate: isVibration  " + isHapticFeedbackEnabled(MainActivity.this));
-        if (!isHapticFeedbackEnabled(MainActivity.this)) {
-            Toast.makeText(MainActivity.this, "Please Turn on Vibrate on tap in Global setting", Toast.LENGTH_SHORT).show();
-        }
+
+        default_sound_vibration.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                if (default_sound_vibration.getText().toString().equals("Default sound + vibration ON")) {
+                    default_sound_vibration.setText("Default sound + vibration OFF");
+                    default_sound_vibration.setBackgroundColor(Color.RED);
+                    Settings.System.putInt(getContentResolver(), Settings.System.SOUND_EFFECTS_ENABLED, 1);
+                    Settings.System.putInt(getContentResolver(), Settings.System.HAPTIC_FEEDBACK_ENABLED, 1);
+                } else { /*(default_sound_vibration.getText().toString().equals("Default sound + vibration OFF")){*/
+                    default_sound_vibration.setText("Default sound + vibration ON");
+                    default_sound_vibration.setBackgroundColor(Color.GREEN);
+                    Settings.System.putInt(getContentResolver(), Settings.System.SOUND_EFFECTS_ENABLED, 0);
+                    Settings.System.putInt(getContentResolver(), Settings.System.HAPTIC_FEEDBACK_ENABLED, 0);
+                }
+
+            }
+        });
 
         default_Vibration.setOnTouchListener(new View.OnTouchListener() {
             @RequiresApi(api = Build.VERSION_CODES.O_MR1)
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
+            public boolean onTouch(final View v, MotionEvent event) {
+
+                boolean isVibrateOnTouchEnabled = Settings.System.getInt(getContentResolver(),
+                        Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
 
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
 
+                        if (!isVibrateOnTouchEnabled) {
+                            isHapticFeedbackEnabled(MainActivity.this, v);
+                        }
                         // For default Vibration
-                        v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
-
+                        else
+                            v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                         break;
 
                     case MotionEvent.ACTION_UP:
@@ -83,23 +109,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-       /* default_Vibration.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // For default Vibration
-                v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY,
-                        HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING);
-            }
-        });*/
-
         d_long_press.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public boolean onLongClick(View v) {
-                if (!isHapticFeedbackEnabled(MainActivity.this)) {
-                    Toast.makeText(MainActivity.this, "Please Turn on Vibrate on tap in Global setting", Toast.LENGTH_SHORT).show();
-                }
-                v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+            public boolean onLongClick(final View v) {
+
+                boolean isVibrateOnTouchEnabled = Settings.System.getInt(getContentResolver(),
+                        Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
+
+                Log.i(TAG, "onTouch: isVibrateOnTouchEnabled " + isVibrateOnTouchEnabled);
+
+                if (!isVibrateOnTouchEnabled) {
+                    isHapticFeedbackEnabled(MainActivity.this, v);
+                } else
+                    v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
                 return false;
             }
         });
@@ -107,27 +129,37 @@ public class MainActivity extends AppCompatActivity {
         default_Vibration_sound.setOnTouchListener(new View.OnTouchListener() {
             @RequiresApi(api = Build.VERSION_CODES.O_MR1)
             @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                if (!isHapticFeedbackEnabled(MainActivity.this)) {
-                    Toast.makeText(MainActivity.this, "Please Turn on Vibrate on tap in Global setting", Toast.LENGTH_SHORT).show();
-                } else if (!isSoundEffectConstants(MainActivity.this)) {
-                    Toast.makeText(MainActivity.this, "Please Turn on Touch Sound in Global setting ", Toast.LENGTH_SHORT).show();
-                    Log.i(TAG, "isSoundEffectConstants: sound.....");
-                } else
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_DOWN:
+            public boolean onTouch(final View view, MotionEvent event) {
 
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+
+                        boolean isVibrateOnTouchEnabled = Settings.System.getInt(getContentResolver(),
+                                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
+
+                        boolean isTouchSoundsEnabled = Settings.System.getInt(getContentResolver(),
+                                Settings.System.SOUND_EFFECTS_ENABLED, 1) != 0;
+
+                        Log.i(TAG, "onTouch: isVibrateOnTouchEnabled " + isVibrateOnTouchEnabled);
+                        settingPermission();
+                        if (!isVibrateOnTouchEnabled) {
+                            isHapticFeedbackEnabled(MainActivity.this, view);
+
+                        } else if (!isTouchSoundsEnabled) {
+                            isSoundEffectConstants(MainActivity.this, view);
+
+                        } else {
                             // For default Vibration
                             view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
 
                             // For default Sound
                             view.playSoundEffect(SoundEffectConstants.CLICK);
+                        }
+                        break;
 
-                            break;
-
-                        case MotionEvent.ACTION_UP:
-                            break;
-                    }
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
                 return true;
             }
         });
@@ -224,16 +256,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         vib_pattern2.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View v) {
                 v.setSoundEffectsEnabled(false);
                 long[] vv1 = {0, 100, 200, 100, 1000, 100, 200, 100};
                 int[] vv = {0, 255, 0, 1, 0, 255, 0, 1};
-                if (Action_code == 1)
-                    myVib.vibrate(VibrationEffect.createWaveform(vv1, vv, -1));
-                Log.i(TAG, "onClick: "+myVib.hasAmplitudeControl());
-
+                if (Action_code == 1) {
+                    // Vibrate method for below API Level 26
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        myVib.vibrate(VibrationEffect.createWaveform(vv1, vv, -1));
+                        Log.i(TAG, "onClick: " + myVib.hasAmplitudeControl());
+                    } else {
+                        long[] mVibratePattern = new long[]{0, 100, 200, 100, 1000, 100, 200, 100};
+                        myVib.vibrate(mVibratePattern, -1);
+                    }
+                }
             }
         });
         vibrate_sound.setOnClickListener(new View.OnClickListener() {
@@ -250,54 +287,117 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void settingPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
+            if (!Settings.System.canWrite(getApplicationContext())) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, 200);
 
-    public static boolean isSoundEffectConstants(Context context) {
-        int enabled = Settings.System.getInt(context.getContentResolver(),
-                Settings.System.SOUND_EFFECTS_ENABLED, 0);
-
-        return enabled != 0;
+            }
+        }
     }
 
-    public static boolean isHapticFeedbackEnabled(Context context) {
-        int enabled = Settings.System.getInt(context.getContentResolver(),
-                android.provider.Settings.System.HAPTIC_FEEDBACK_ENABLED, 0);
-        return enabled != 0;
+    private boolean checkSystemWritePermission() {
+        boolean retVal = true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            retVal = Settings.System.canWrite(this);
+            Log.d(TAG, "Can Write Settings: " + retVal);
+            if (retVal) {
+//                Toast.makeText(this, "Write allowed :-)", Toast.LENGTH_LONG).show();
+            } else {
+//                Toast.makeText(this, "Write not allowed :-(", Toast.LENGTH_LONG).show();
+            }
+        }
+        return retVal;
+    }
+
+
+    public void isSoundEffectConstants(Context context, final View v) {
+        settingPermission();
+        if (checkSystemWritePermission()) {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this);
+
+            dialogBuilder.setMessage("Do you want to turn on Touch on Sound ?");
+            dialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Settings.System.putInt(getContentResolver(), Settings.System.SOUND_EFFECTS_ENABLED, 1);
+                    v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                    v.playSoundEffect(SoundEffectConstants.CLICK);
+                }
+            });
+            dialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            AlertDialog alertDialog = dialogBuilder.create();
+            alertDialog.show();
+        }
+    }
+
+    public void isHapticFeedbackEnabled(Context context, final View v) {
+
+        settingPermission();
+        if (checkSystemWritePermission()) {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+
+            dialogBuilder.setMessage("Do you want to turn on Touch on Vibrate ?");
+            dialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Settings.System.putInt(getContentResolver(), Settings.System.HAPTIC_FEEDBACK_ENABLED, 1);
+                    v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                    isisHapticFeedbackEnabled = true;
+                }
+            });
+            dialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            AlertDialog alertDialog = dialogBuilder.create();
+            alertDialog.show();
+
+        }
     }
 
     public void success(Vibrator vibrator) {
-        if(vibrator.hasVibrator())
-        vibrator.vibrate(50);
+        if (vibrator.hasVibrator())
+            vibrator.vibrate(50);
     }
 
     public void warning(Vibrator vibrator) {
-        if(vibrator.hasVibrator())
-        vibrator.vibrate(250);
+        if (vibrator.hasVibrator())
+            vibrator.vibrate(250);
     }
 
     public void error(Vibrator vibrator) {
-        if(vibrator.hasVibrator())
-        vibrator.vibrate(500);
+        if (vibrator.hasVibrator())
+            vibrator.vibrate(500);
     }
 
     public void light(Vibrator vibrator) {
-        if(vibrator.hasVibrator())
-        vibrator.vibrate(20);
+        if (vibrator.hasVibrator())
+            vibrator.vibrate(20);
     }
 
     public void medium(Vibrator vibrator) {
-        if(vibrator.hasVibrator())
-        vibrator.vibrate(50);
+        if (vibrator.hasVibrator())
+            vibrator.vibrate(50);
     }
 
     public void heavy(Vibrator vibrator) {
-        if(vibrator.hasVibrator())
-        vibrator.vibrate(100);
+        if (vibrator.hasVibrator())
+            vibrator.vibrate(100);
     }
 
     public void selection(Vibrator vibrator) {
-        if(vibrator.hasVibrator())
-        vibrator.vibrate(20);
+        if (vibrator.hasVibrator())
+            vibrator.vibrate(20);
     }
 
 
